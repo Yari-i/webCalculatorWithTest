2ade6d4efda59820adf0f386ceae91dfffa15fa8
Yari-i <tipo47181@gmail.com>
2024-12-01 20:43:52 +0500

    Start homework

---
+++ src/test/java/ru/skypro/webCalculatorDemo/WebCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.webCalculatorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
